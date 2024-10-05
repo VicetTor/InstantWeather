40c92aa7e1aa7b6bea890fcb11437b8d04be8a44
Louane KDUAL <louanekdual@gmail.com>
2024-10-05 18:40:41 +0200

    feat : récupération données pour 7 jours selon code insee

---
+++ script.js
@@ -0,0 +1,26 @@
+let dataPerDay = [];
+let dataWeather;
+
+async function setDataWeather(codeInsee) {
+    const url = `https://api.meteo-concept.com/api/forecast/daily?token=4fc5437cc97af368607aa51c5e24da9d2d95835be19cd8fecb0d37d29a0c3382&insee=${codeInsee}`;
+    try {
+      const response = await fetch(url);
+      dataWeather = await response.json();
+      setDataPerDay();
+
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+
+function setDataPerDay(){
+    for (let i = 0; i<7; i++){
+        dataPerDay[i] = dataWeather.forecast[i];
+        console.log(dataPerDay[i]);
+    }
+    test()
+}
+
+setDataWeather(50215);
+
+//setTimeout(test,1000);
