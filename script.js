cdbd84ec0a36389de5f9d9de38cede3ed728dbaf
BEUVE <beuve225@unicaen.fr>
2024-10-08 23:41:30 +0200

    feat: pluie sur la page d'acceuil

--- script.js
+++ script.js
@@ -404,5 +404,58 @@ function verifCheckBox(checkBox,value,text){
 
 
 
+const canvas = document.getElementById('rainfall');
+const ctx = canvas.getContext('2d');
 
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const raindrops = [];
+
+function createRainDrop(){
+    const x = Math.random() * canvas.width
+    const y = -5;
+    const speed = Math.random() * 5 + 2;
+    const length = Math.random() * 20 + 10;
+
+    raindrops.push({x, y, speed, length});
+}
+
+function updateRaindrops(){
+    for(let i = 0; i < raindrops.length; i++){
+        const raindrop = raindrops[i];
+
+        raindrop.y += raindrop.speed;
+
+        if(raindrop.y > canvas.height){
+            raindrops.splice(i,1);
+            i--;
+        }
+    }
+}
+
+function drawRaindrops(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = 2;
+
+    for(let i = 0; i<raindrops.length; i++){
+        const raindrop = raindrops[i];
+
+        ctx.beginPath();
+        ctx.moveTo(raindrop.x, raindrop.y);
+        ctx.lineTo(raindrop.x, raindrop.y + raindrop.length);
+        ctx.stroke();
+    }
+
+}
+
+function animate(){
+    createRainDrop();
+    updateRaindrops();
+    drawRaindrops();
+
+    requestAnimationFrame(animate);
+}
 
