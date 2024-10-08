2c74463b48eb26b02b4491445f7a7677128efa7b
Victor <victoryvray@gmail.com>
2024-10-08 11:53:43 +0200

    feat: Ajout des descriptions météo

--- script.js
+++ script.js
@@ -208,8 +208,6 @@ async function fetchDataMeteo(codeInsee){
     }
 }
 
-
-
 function affichageInfos(){
     infoMain.style.visibility = "visible";
     enleverAffichageCommune();
@@ -299,36 +297,46 @@ function weatherDescriptions (weather, s){
     if(weather == 0){
         s.innerHTML = '<i class="fa-regular fa-sun"></i>';
         if (s == sky){ body.style.backgroundColor ="#80DDE3"; 
-          formulaire.style.backgroundColor ="#80DDE3"; }
+          formulaire.style.backgroundColor ="#80DDE3";
+          skyDescription.innerText = "Ensoleillé"
+        }
     } 
     if((weather >= 1 && weather <= 5) || (weather == 16) ){
         s.innerHTML = '<i class="fa-solid fa-cloud"></i>';
         if (s == sky){ body.style.backgroundColor="#6FB8BD";
-          formulaire.style.backgroundColor ="#6FB8BD"; }
+          formulaire.style.backgroundColor ="#6FB8BD"; 
+          skyDescription.innerText = "Nuageux"
+        }
     }
     if(weather >= 6 && weather <= 7 ){
         s.innerHTML = '<i class="fa-solid fa-smog"></i>';
         if (s == sky){ body.style.backgroundColor = "#59989C";
-          formulaire.style.backgroundColor ="#59989C"; }
+          formulaire.style.backgroundColor ="#59989C"; 
+          skyDescription.innerText = "Brumeux"
+        }
     }
     if((weather >= 10 && weather <= 15) || (weather >= 40 && weather <= 48) || (weather >= 210 && weather <= 212) || (weather == 235)){
         s.innerHTML =  '<i class="fa-solid fa-cloud-rain"></i>';
         if (s == sky){ 
           body.style.backgroundColor = "#496769";
           formulaire.style.backgroundColor ="#496769";
-        
+          skyDescription.innerText = "Pluvieux"
         }
     }
     if((weather >= 20 && weather <= 22 ) || (weather >= 30 && weather <= 32) ||  (weather >= 60 && weather <= 68) || (weather >= 70 && weather <= 78) || (weather >= 220 && weather <= 2022) || (weather >= 230 && weather <= 232)){
         s.innerHTML = '<i class="fa-solid fa-snowflake"></i>'
         if (s == sky){ body.style.backgroundColor = "#8BA1A3"; 
-        formulaire.style.backgroundColor ="#8BA1A3"; }
+        formulaire.style.backgroundColor ="#8BA1A3"; 
+        skyDescription.innerText = "Enneigé"
+    }
     }
     if((weather >= 100 && weather <= 108) || (weather >= 120 && weather <= 142)){
         s.innerHTML = '<i class="fa-solid fa-poo-storm"></i>';
         if (s == sky){ 
           body.style.backgroundColor = "#302A2A"; 
-          formulaire.style.backgroundColor ="#302A2A"; }  
+          formulaire.style.backgroundColor ="#302A2A";
+          skyDescription.innerText = "Orageux"
+        }  
     }     
 }
 
